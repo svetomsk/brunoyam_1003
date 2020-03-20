28841d1ed7cc4a14ba8aa0b169040359fcc1ef24
Milykh Svetozar <svt2404@gmail.com>
2020-03-20 13:33:13 +0300

    Quiz conditions, numbers, string

---
+++ quiz/FirstQuiz.java
@@ -0,0 +1,30 @@
+public class FirstQuiz {
+    public static void main(String[] args) {
+        int age = 24;
+        // проверить, зная возраст, что человек совершеннолетний
+        if(...) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        String name = "Svetozar";
+        // напечатать длину имени
+        System.out.println("Количество букв в имени " + name + " = " + ...);
+
+        String phone = "+79998887766";
+        // проверить, что телефон начинается с кода +7 и состоит из 10 цифр
+        if(...) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String patronymic = "Ivanovich";
+        // составить строчку из фамилии и инициалов: Ivanov I. I.
+        String fullName = ...;
+        System.out.println(fullName);
+    }
+}
