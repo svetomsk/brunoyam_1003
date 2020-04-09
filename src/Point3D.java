57323c06947434ad588367475fec4224892c8be5
svetomsk <svt2404@gmail.com>
2020-04-09 22:11:56 +0300

    point3d almost complete

--- src/Point3D.java
+++ src/Point3D.java
@@ -10,6 +10,21 @@ public class Point3D {
         z = newZ;
     }
 
+    public double distToZero() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public String getStringValue() {
+        String result = "(" + x + ", " + y + ", " + z + ")";
+        return result;
+    }
+
+//    public double distance(int x1, int y1, int z1, int x2, int y2, int z2) {
+    public double distance(Point3D other) {
+        return Math.sqrt(Math.pow(x - other.x, 2) +
+                Math.pow(y - other.y, 2) + Math.pow(z - other.z, 2));
+    }
+
     // три координаты (x, y, z) - могут быть дробные
     // расстояние до (0, 0, 0) sqrt(x*x + y*y + z*z)
     // getStringValue() -> (1.2, 2.3, 2.3)
@@ -24,9 +39,10 @@ class PointsTest {
         System.out.println(first.distToZero());
 
         Point3D second = new Point3D(2, 2, 2);
+        System.out.println(second.distToZero());
         System.out.println(first.distance(second));
         // альтернатива
-        System.out.println(Point3D.distance(first, second));
+//        System.out.println(Point3D.distance(first, second));
     }
 }
 
