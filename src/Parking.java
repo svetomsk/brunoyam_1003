bd3eefd20e69d5b29dbca0edb64156be8ec096e4
svetomsk <svt2404@gmail.com>
2020-04-13 14:55:17 +0300

    pre solution code

--- src/Parking.java
+++ src/Parking.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Parking {
     String [] parking;
     int size;
@@ -5,10 +7,23 @@ public class Parking {
     public Parking(int n) {
         size = n;
         parking = new String[size];
+        for (int i = 0; i < n; i++) {
+            parking[i] = "";
+        }
     }
 
     public void addCar(String number) {
+        int pos = firstFreePlace();
+        parking[pos] = number;
+    }
 
+    private int firstFreePlace() {
+        for (int i = 0; i < size; i++) {
+            if(parking[i].equals("")) { // !parking[i].isEmpty()
+               return i;
+            }
+        }
+        return -1;
     }
 
     public void freeCar(String number) {
@@ -16,7 +31,7 @@ public class Parking {
     }
 
     public void show() {
-
+        System.out.println(Arrays.toString(parking));
     }
 
     public int freePlaces() {
